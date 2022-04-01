fa6392b3cbc263f440a5a8fe234f4e03e1da774d
KristianColville1 <kristiancolville1@gmail.com>
2022-04-01 15:50:07 +0100

    Refractor welcome message inside function print_welcome()

--- run.py
+++ run.py
@@ -4,10 +4,13 @@ Tetromino Falls is terminal based game displayed in the browser.
 import curses
 from curses import wrapper
 
+# Initializes the curses objects
+curses.initscr()
 
-stdscr = curses.initscr()
+# Allows the curses colors to be accessed and used
 curses.start_color()
-# background curses colors initialized
+
+# background curses color pairs initialized
 curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
 curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_CYAN)
 curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_RED)
@@ -16,7 +19,7 @@ curses.init_pair(5, curses.COLOR_WHITE, curses.COLOR_GREEN)
 curses.init_pair(6, curses.COLOR_WHITE, curses.COLOR_MAGENTA)
 curses.init_pair(7, curses.COLOR_WHITE, curses.COLOR_WHITE)
 
-# text curses colors initialized
+# text curses color pairs initialized
 curses.init_pair(8,  curses.COLOR_BLUE, curses.COLOR_BLACK)
 curses.init_pair(9,  curses.COLOR_CYAN, curses.COLOR_BLACK)
 curses.init_pair(10, curses.COLOR_RED, curses.COLOR_BLACK)
@@ -25,7 +28,7 @@ curses.init_pair(12, curses.COLOR_GREEN, curses.COLOR_BLACK)
 curses.init_pair(13, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
 curses.init_pair(14, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-# background colors with white text
+# background colors with white text assigned to constants
 BLUE = curses.color_pair(1)
 CYAN = curses.color_pair(2)
 RED = curses.color_pair(3)
@@ -34,7 +37,7 @@ GREEN = curses.color_pair(5)
 PURPLE = curses.color_pair(6)
 WHITE = curses.color_pair(7)
 
-# text colors with black bg
+# text colors with black bg assigned to constants
 BLUE_TEXT = curses.color_pair(8)
 CYAN_TEXT = curses.color_pair(9)
 RED_TEXT = curses.color_pair(10)
@@ -42,6 +45,32 @@ YELLOW_TEXT = curses.color_pair(11)
 GREEN_TEXT = curses.color_pair(12)
 PURPLE_TEXT = curses.color_pair(13)
 WHITE_TEXT = curses.color_pair(14)
+
+def print_welcome(welcome_window):
+    """
+    When called upon this function prints the first menu the user encounters.
+    It waits for the user to press a key before moving on with the program.
+    """
+    file = open('welcome-msg.txt')
+    text = file.read()
+    file.close()
+
+    welcome_window.addstr(f'{text}', CYAN_TEXT)
+    welcome_window.addstr(18, 25, 'Created by Kristian Colville', YELLOW_TEXT)
+    welcome_window.addstr(22, 15, 'Press any key to continue..', RED_TEXT)
+
+    # gets the users input and exits this function
+    welcome_window.getch()
+
+
+def get_user_name():
+    """
+    Gets the users name and checks the data base for names entered.
+    It returns the users name for displaying on the screen.
+    """
+    user = ''
+    return user
+
 
 def main(stdscr):
     """
@@ -52,14 +81,9 @@ def main(stdscr):
     stdscr.clear()
 
     welcome_window = curses.newwin(29, 79)
-    file = open('welcome-msg.txt')
-    text = file.read()
-    file.close()
+    print_welcome(welcome_window)
+    user = get_user_name()
 
-    welcome_window.addstr(f'{text}', CYAN_TEXT)
-    welcome_window.addstr(18, 25, 'Created by Kristian Colville', YELLOW_TEXT)
-    welcome_window.addstr(22, 15, 'Press any key to continue..', RED_TEXT)
-    welcome_window.getch()
 
 # creates an object to initialize the curser objects using the main function
 wrapper(main)
