39fa63dc2f90f09a6407e1de46958ed75a27d12d
KristianColville1 <kristiancolville1@gmail.com>
2022-04-07 10:17:39 +0100

    Add instructions method to class message

--- tetromino/tetromino/message.py
+++ tetromino/tetromino/message.py
@@ -1,15 +1,14 @@
 """Handles printing text to terminal for Tetromino Falls"""
+import time
 from tetromino.tetromino import console
-class Message():
+class Message:
     """
     Creates a message object that holds different methods for
     printing to terminal.
     """
     def __init__(self):
         self.welcome = self.get_welcome()
-        self.instructions = self.get_instructions()
-        self.goodbye = self.get_goodbye()
-
+        self.get_next_action = self.get_next_action()
 
     def get_welcome(self):
         """
@@ -19,6 +18,7 @@ class Message():
             file = open('welcome-msg.txt', encoding='utf8')
             self.welcome = file.read()
             file.close()
+            
             if self.welcome is None:
                 raise IOError(
                     'An error occurred fetching the welcome message.. sorry about that..'
@@ -27,7 +27,12 @@ class Message():
             console.clear_console()
             print(f'Oops something went wrong!\n\033[31m{error}\033[0m')
 
-        return print('\n\n\n\033[31m' + self.welcome + '\033[0m\n\n\n')
+        try:
+            print('\n\n\n\033[31m' + self.welcome + '\033[0m\n\n\n')
+            input('\t\t\t\033[36mHit enter to continue...\033[0m')
+        except ValueError:
+            self.get_welcome()
+        return True
 
 
     def get_instructions(self):
@@ -35,12 +40,43 @@ class Message():
         Prints the instructions to the terminal for the user when
         called for the Tetromino Falls game.
         """
-        return ''
+        print('\033[0;33m')
+        try:
+            console.clear_console()
+            file = open('instructions.txt', encoding='utf8')
+            instructions = file.read()
+            file.close()
+            try:
+                read_instructions = input('\033[31m\nEnter "y" and hit enter to continue: \033[0m')
+                if read_instructions not in ('Y', 'y'):
+                    raise ValueError(
+                        '\033[36m\nEnter the letter y for yes or n for no\n\033[0m'
+                    )
+            except ValueError as error:
+                print(error)
+                time.sleep(2)
+                console.clear_console()
+            if instructions is None:
+                raise IOError(
+                    "Failed to read instructions from welcome.txt..."
+                )
+        except IOError as error:
+            console.clear_console()
+            print(f'\033[31m\n{error}\033[0m')
+        return print(f"{instructions}\033[0m")
 
 
     def get_goodbye(self):
         """
         Prints the goodbye message to the terminal for the user for the
         Tetromino Falls game.
+        """
+        return ''
+
+
+    def get_next_action(self):
+        """
+        Prints options to terminal for user to decide what route to take
+        in Tetromino Falls program.
         """
         return ''
