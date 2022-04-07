fe3d7bb0daa66069032b16a6f476f5eac3a6c6b4
KristianColville1 <kristiancolville1@gmail.com>
2022-04-07 08:49:28 +0100

    Add method to write welcome to terminal

--- tetromino/tetromino/message.py
+++ tetromino/tetromino/message.py
@@ -1,5 +1,5 @@
 """Handles printing text to terminal for Tetromino Falls"""
-
+from tetromino.tetromino import console
 class Message():
     """
     Creates a message object that holds different methods for
@@ -15,7 +15,19 @@ class Message():
         """
         Prints the welcome message to terminal when called.
         """
-        return ''
+        try:
+            file = open('welcome-msg.txt', encoding='utf8')
+            self.welcome = file.read()
+            file.close()
+            if self.welcome is None:
+                raise IOError(
+                    'An error occurred fetching the welcome message.. sorry about that..'
+                )
+        except IOError as error:
+            console.clear_console()
+            print(f'Oops something went wrong!\n\033[31m{error}\033[0m')
+
+        return print('\n\n\n\033[31m' + self.welcome + '\033[0m\n\n\n')
 
 
     def get_instructions(self):
