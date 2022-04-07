c4355a5ca26b6eb4fbe23639b2008d5ad737bf1f
KristianColville1 <kristiancolville1@gmail.com>
2022-04-07 08:39:23 +0100

    Add methods for welcome,instructions and goodbye text

--- tetromino/tetromino/message.py
+++ tetromino/tetromino/message.py
@@ -9,5 +9,26 @@ class Message():
         self.welcome = self.get_welcome()
         self.instructions = self.get_instructions()
         self.goodbye = self.get_goodbye()
-    
-    
+
+
+    def get_welcome(self):
+        """
+        Prints the welcome message to terminal when called.
+        """
+        return ''
+
+
+    def get_instructions(self):
+        """
+        Prints the instructions to the terminal for the user when
+        called for the Tetromino Falls game.
+        """
+        return ''
+
+
+    def get_goodbye(self):
+        """
+        Prints the goodbye message to the terminal for the user for the
+        Tetromino Falls game.
+        """
+        return ''
